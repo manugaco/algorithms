2bf69bfe0295d9d09086815e6163d00e89a6936a
Manu García <manu.garcia.corbi@gmail.com>
2019-09-30 15:17:56 +0200

    Update knn.R

--- knn.R
+++ knn.R
@@ -6,14 +6,14 @@
 #If the confusion matrix is not squared, it does not print the previous information.
 
 
-#I use the Iris dataset as an example.
-
+#I have used the Iris dataset as an example.
 data(iris)
+
 data <- iris
 
 #Implementation of knn (R)
 
-knn_manu <- function(data, k="Auto", splitTT = 0.75, Acc = TRUE){
+knn_manu <- function(data, splitmode = TRUE, k="Auto", splitTT=0.75, Acc = TRUE){
   
   #Avoid more than one factor variable
   
@@ -25,7 +25,7 @@ knn_manu <- function(data, k="Auto", splitTT = 0.75, Acc = TRUE){
   }
   
   if(nonum > 1){
-    stop("There is more than one grouping variable, select only one as input.")
+    stop("There is more than one grouping variable.")
   }
   
   #Detect labels variable (convert to factor)
@@ -37,44 +37,86 @@ knn_manu <- function(data, k="Auto", splitTT = 0.75, Acc = TRUE){
     }
   }
   
-  #Divide in train and test
-  
-  ind <- sample(1:nrow(data), size = round(splitTT*nrow(data)))
-  train <- dataset[ind,]
-  test <- dataset[-ind,]
-  
-  if (k == "Auto"){
-    k <- round(sqrt(nrow(train) + nrow(test)))
-  }
-  
-  #algorithm body
-  
-  distmat <- matrix(0, ncol = nrow(test), nrow = nrow(train))
-  newlabs <- c()
-  
-  for(i in 1:nrow(test)){
-    dist <- numeric()
-    for(j in 1:nrow(train)){
-      dist[j] <- sqrt((sum(test[i,-ind_fact] - train[j,-ind_fact]))^2)
+  if(splitmode == TRUE){
+    
+    #Divide in train and test
+    
+    ind <- sample(1:nrow(data), size = round(splitTT*nrow(data)))
+    train <- dataset[ind,]
+    test <- dataset[-ind,]
+    
+    if (k == "Auto"){
+      k <- round(sqrt(nrow(train) + nrow(test)))
     }
-    distmat[,i] <- dist
-    ord <- order(distmat[,i])
-    ordind <- ord[1:k]
-    data[ordind,ind_fact]
-    newlabs[i] <- names(sort(table(data[ordind,ind_fact]),decreasing=TRUE)[1]) #Selected category
-  }
-  
-  results <- list(distmat, newlabs)
-  
-  if(Acc == TRUE){
-    if (!require("caret")) install.packages("caret")
-    library(caret)
+    
+    #algorithm body
+    
+    distmat <- matrix(0, ncol = nrow(test), nrow = nrow(train))
+    newlabs <- c()
+    
+    for(i in 1:nrow(test)){
+      dist <- numeric()
+      for(j in 1:nrow(train)){
+        dist[j] <- sqrt((sum(test[i,-ind_fact] - train[j,-ind_fact]))^2)
+      }
+      distmat[,i] <- dist
+      ord <- order(distmat[,i])
+      ordind <- ord[1:k]
+      data[ordind,ind_fact]
+      newlabs[i] <- names(sort(table(data[ordind,ind_fact]),decreasing=TRUE)[1]) #Selected category
+    }
+    
+    results <- list(distmat, newlabs)
     confmat <- table(newlabs, test[,ind_fact])
-    cm <- invisible(force(confusionMatrix(confmat)))
-    results[[3]] <- cm
+    
+    if(Acc == TRUE && (ncol(confmat)==nrow(confmat))){
+      acc <- sum(confmat[row(confmat) == col(confmat)])/(sum(confmat[row(confmat) != col(confmat)]) + sum(confmat[row(confmat) == col(confmat)]))
+      results[[3]] <- paste("Accuracy", acc, sep = " ")
+    }
+    
+    return(results)
+    
+  }else{
+    
+    if (k == "Auto"){
+      k <- round(sqrt(nrow(data)))
+    }
+    
+    #algorithm body
+    
+    distmat <- matrix(0, ncol = (nrow(data)), nrow = (nrow(data)))
+    newlabs <- c()
+    
+    for(i in 1:nrow(data)){
+      dist <- numeric()
+      for(j in 1:nrow(data)){
+        if(i != j){
+        dist[j] <- sqrt((sum(data[i,-ind_fact] - data[j,-ind_fact]))^2)
+        }else{
+        dist[j] <- 99999999999
+        }
+      } 
+      distmat[i,] <- dist
+      ord <- order(distmat[i,])
+      ordind <- ord[1:k]
+      data[ordind,ind_fact]
+      newlabs[i] <- names(sort(table(data[ordind,ind_fact]),decreasing=TRUE)[1]) #Selected category
+    }
+    
+    results <- list(distmat, newlabs)
+    
+    confmat <- table(newlabs, data[,ind_fact])
+    
+    if(Acc == TRUE && (ncol(confmat)==nrow(confmat))){
+      acc <- sum(confmat[row(confmat) == col(confmat)])/(sum(confmat[row(confmat) != col(confmat)]) + sum(confmat[row(confmat) == col(confmat)]))
+      results[[3]] <- paste("Accuracy", acc, sep = " ")
+    }
+    
+    return(results)
   }
   
-  return(results)
 } #Knn function
 
-results <- knn_manu(iris, k = 3)
+results <- knn_manu(iris, splitmode = TRUE, splitTT = 0.5, Acc = TRUE)
+
+
