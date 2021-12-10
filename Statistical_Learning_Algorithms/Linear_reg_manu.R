32417bd54be72a46befb2c0716ffe943205813a1
majogar <manuel.garcia@idrica.com>
2021-12-10 20:17:45 +0100

    improvements

--- Statistical_Learning_Algorithms/Linear_reg_manu.R
+++ Statistical_Learning_Algorithms/Linear_reg_manu.R
@@ -1,7 +1,13 @@
 
+## Statistics:
+
+# Mean:
+
 f_mean <- function(x){
     return(sum(x)/length(x))
 }
+
+# Sum of squares:
 
 f_SS <- function(x){
     aux <- c()
@@ -12,7 +18,10 @@ f_SS <- function(x){
     return(sum(aux))
 }
 
+# Covariance:
+
 f_cov <- function(x, y){
+
     mean_x <- f_mean(x)
     mean_y <- f_mean(y)
     cov <- c()
@@ -27,6 +36,18 @@ f_cov <- function(x, y){
     return(sum(cov))
 }
 
+# Function to split a dataframe between features and target:
+
+f_stf <- function(data, target){
+
+    X <- c(data[, names(data) != target])
+    y <- c(data[, names(data) == target])
+
+    return(list(X, y))
+}
+
+# Simple linear regression coefficients:
+
 f_beta1 <- function(x, y){
     return(f_cov(x, y)/f_SS(x))
 }
@@ -34,6 +55,26 @@ f_beta1 <- function(x, y){
 f_beta0 <- function(x, y){
     return(f_mean(y) - (f_beta1(x, y)*f_mean(x)))
 }
+
+# Function to get the coefficients from training data:
+
+f_srl_fit <- function(df_train, target){
+
+    coef <- c()
+    ft_list = f_stf(df_train, target)
+    x_train <- ft_list[[1]]
+    y_train <- ft_list[[2]]
+    b1 <- f_beta1(x_train, y_train)
+    coef <- append(b1, f_beta0(x_train, y_train))
+    
+    return(coef)
+}
+
+# Function to predict the y values given test data:
+
+
+
+# Function to get the mean squared error:
 
 f_mse <- function(a,p){
     s_error <- c()
@@ -49,6 +90,8 @@ f_mse <- function(a,p){
     return(f_mean(s_error))
 }
 
+# Function to get the root mean squared error:
+
 f_rmse <- function(a, p){
     if(length(a)!=length(p)){
         print("Error, actual and predicted values vectors must have same length.")
@@ -57,20 +100,34 @@ f_rmse <- function(a, p){
     return(f_mse(a, p)**(1/2))
 }
 
-f_simple_linear_regression <- function(train, test){
+# Function to get the mean absolute error:
 
-    preds <- c()
+f_mae <- function(a,p){
+    s_error <- c()
 
-    b1 <- f_beta1(x, y)
-    b0 <- f_beta0(x, y)
+    if(length(a)!=length(p)){
+        print("Error, actual and predicted values vectors must have same length.")
+    }else{
+        for(i in 1:length(a)){
+            s_error[i] <- abs(a[i] - p[i])
+        }
 
-
+    }
+    return(f_mean(s_error))
 }
 
 # Testing functions:
 
 x <- c(1, 2, 4, 3, 5)
 y <- c(1, 3, 3, 2, 5)
+
+df_train = data.frame(y, x)
+df_train
+
+x_test <- c(1, 2, 2, 1, 5)
+y_test <- c(1, 3, 2, 1, 4)
+
+df_test <- data.frame()
 
 f_mean(x)
 f_mean(y)
@@ -81,9 +138,13 @@ f_SS(y)
 f_cov(x, y)
 
 f_beta1(x, y)
-
 f_beta0(x, y)
 
-# Predictions:
+coef <- f_srl_fit(df_train, 'y')
+coef
+preds <- f_slr_predict(df_test, coef)
+preds
 
-
+f_mse(y_test, preds)
+f_rmse(y_test, preds)
+f_mae(y_test, preds)
--- Statistical_Learning_Algorithms/Linear_reg_manu.py
+++ Statistical_Learning_Algorithms/Linear_reg_manu.py
@@ -1,14 +1,24 @@
+
+## Libraries:
+
 import numpy as np
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
 
+## Statistics:
 
-
+# Mean:
 
 def f_mean(x):
     return sum(x) / float(len(x))
 
+# Sum of squares:
+
 def f_SS(x):
     mean = f_mean(x)
     return sum([(i-mean)**2 for i in x])
+
+# Covariance:
 
 def f_cov(x, y):
 
@@ -23,11 +33,51 @@ def f_cov(x, y):
             cov.append(aux)
         return sum(cov)
 
+# Function to split a dataframe between features and target:
+
+def f_stf(data, target):
+
+    X = np.array(data.drop(target, axis=1))
+    y = np.array(data[target])
+
+    return(X, y)
+
+# Simple linear regression coefficients:
+
 def f_beta1(x, y):
     return(f_cov(x, y)/f_SS(x))
 
 def f_beta0(x, y):
     return(f_mean(y) - (f_beta1(x, y)*f_mean(x)))
+
+# Function to get the coefficients from training data:
+
+def f_slr_fit(df_train, target):
+
+    coef = []
+    x_train, y_train = f_stf(df_train, target)
+    coef.append(f_beta0(x_train, y_train)[0])
+    coef.append(f_beta1(x_train, y_train)[0])
+
+    return coef
+
+# Function to predict the y values given test data:
+
+def f_slr_predict(df_test, coef):
+    
+    preds = []
+    x_test = np.array(df_test)
+    b0 = coef[0]
+    b1 = coef[1]
+
+    for i in range(len(x_test)):
+
+        y_hat = b0 + b1*x_test[i]
+        preds.append(y_hat[0])
+
+    return preds
+
+# Function to get the mean squared error:
 
 def f_mse(a, p):
     s_error = []
@@ -40,6 +90,8 @@ def f_mse(a, p):
 
     return(f_mean(s_error))
 
+# Function to get the root mean squared error:
+
 def f_rmse(a, p):
 
     if len(a) != len(p):
@@ -47,19 +99,33 @@ def f_rmse(a, p):
     
     return f_mse(a, p)**(1/2)
 
-def f_simple_linear_regression(train, test):
-    
-    preds = []
+# Function to get the mean absolute error:
 
-    b1 = f_beta1(x, y)
-    b0 = f_beta0(x, y)
+def f_mae(a, p):
 
+    s_error = []
+
+    if len(a) != len(p):
+        exit("Error, actual and predicted values vectors must have same length.")
+    else:
+        for i in range(len(a)):
+            s_error.append(abs(a[i] - p[i]))
+
+    return(f_mean(s_error))
 
 # Testing functions:
 
-dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
-x = [row[0] for row in dataset]
-y = [row[1] for row in dataset]
+x = [1, 2, 4, 3, 5]
+y = [1, 3, 3, 2, 5]
+
+df_train = pd.DataFrame(x, y).reset_index().rename(columns={'index':'y', 0:'x'})
+df_train
+
+x_test = [1, 2, 2, 1, 5]
+y_test = [1, 3, 2, 1, 4]
+
+df_test = pd.DataFrame(x_test).rename(columns={0:'x'})
+df_test
 
 f_mean(x)
 f_mean(y)
@@ -70,9 +136,13 @@ f_SS(y)
 f_cov(x, y)
 
 f_beta1(x, y)
-
 f_beta0(x, y)
 
-# Predictions:
+coef = f_slr_fit(df_train, 'y')
+coef
+preds = f_slr_predict(df_test, coef)
+preds
 
-
+f_mse(y_test, preds)
+f_rmse(y_test, preds)
+f_mae(y_test, preds)
